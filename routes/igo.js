16ca740be9eac067a81d2a4b93f528ef80d53b8e
Iker Ibarguren <iibarguren@gureak.com>
2014-03-20 16:52:57 +0100

    Fitxategi asko batera igotzeko aukera du orain, bodyparse erabiliz

--- routes/igo.js
+++ routes/igo.js
@@ -2,12 +2,27 @@
  * Created by ikerib on 20/03/14.
  */
 
-var uploadHelper = require('./uploadHelper');
-
-exports.igo = function(req, res){
+exports.igo = function (req, res) {
     res.render('igo');
 };
 
-exports.doUpload = function(req,res){
-    uploadHelper.doUpload(req,res);
+exports.doUpload = function (req, res, next) {
+    var fs = require('fs');
+    for (var i = 0; i < req.files.uploadFile.length; i++) {
+
+        // path tenporala
+        var tmp_path = req.files.uploadFile[i].path;
+        // path nora bidali nahi dugun
+        var target_path = './public/images/' + req.files.uploadFile[i].name;
+        // fitxategia mugitu
+        fs.rename(tmp_path, target_path, function (err) {
+            if (err) throw err;
+            // ezabatu fitxategi tenporala
+            fs.unlink(tmp_path, function () {
+                if (err) throw err;
+                res.send('Fitxategia: ' + target_path + ' karpetara igoa izan da.');
+            });
+        });
+    }
+    res.render('egina');
 };
